5af7efe31e52d2d9755bb541142d2ea29f202036
khadar-29 <khadarvalimb@gmail.com>
2025-04-04 00:12:02 +0530

    Save the java

---
+++ Main.java
@@ -0,0 +1,30 @@
+class Person {
+    String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public void display() {
+        System.out.println("Name: " + name);
+    }
+}
+
+class Student extends Person {
+
+    public Student(String name) {
+        super(name);
+    }
+
+    public void study() {
+        System.out.println(name + " is studying.");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student("John");
+        student.display();
+        student.study();
+    }
+}
